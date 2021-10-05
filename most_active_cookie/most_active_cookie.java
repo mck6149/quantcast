e7659750553ab8e962b5c4a55324b506f61b5082
Maxwell Kimber <maxwellk@a30357maxwellk.us.corp>
2021-10-05 19:40:12 -0400

    added maven as framework support so that I can easily produce jar files

--- most_active_cookie/most_active_cookie.java
+++ most_active_cookie/most_active_cookie.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class most_active_cookie{
 
 	public static void main(String args[]){
-		System.out.println(args[1] + " " + args[2]);
+		//System.out.println(args[1] + " " + args[2]);
+		System.out.println("My arguments here");
 	}
 }
