2bab8195dbe94f1ec04f4fae42f9e263e5988ee5
Maxwell Kimber <maxwellk@a30357maxwellk.us.corp>
2021-10-05 19:31:17 -0400

    added prompt

--- MostActiveCookie/MostActiveCookie.java
+++ MostActiveCookie/MostActiveCookie.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class MostActiveCookie{
 
 	public static void main(String args[]){
-		System.out.println("Hello Cookies\n");
+		System.out.println(args[1] + " " + args[2]);
 	}
 }
